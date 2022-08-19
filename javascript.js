af29f96e5f4f0747d10a0b6bfa941762c1f27f0e
michael-chung-mc <chung.michael.mc@gmail.com>
2022-08-19 19:28:35 -0400

    Make number buttons update display

--- javascript.js
+++ javascript.js
@@ -26,3 +26,23 @@ function operate (x,y, operator)
     else if (operator == '/') {return divide(x,y);}
     else {console.log('ERROR: unsupported operator')}
 }
+
+function updateDisplay()
+{
+    display.innerText = displayValue;
+}
+
+let displayValue = "";
+let display = document.getElementById('displayText');
+let digits = document.getElementsByClassName('digit');
+for (let i = 0; i < digits.length; i++) 
+{
+    let number = digits[i].innerText;
+    digits[i].addEventListener('click',() => {
+        // console.log(displayValue);
+        displayValue = displayValue + number
+        updateDisplay();
+    });
+}
+updateDisplay();
+
