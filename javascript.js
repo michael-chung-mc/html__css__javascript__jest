4266717a7e7d71d98b9c75a25e7aef3d0681ef82
michael-chung-mc <chung.michael.mc@gmail.com>
2022-08-19 19:39:49 -0400

    Add basic operation functionality

--- javascript.js
+++ javascript.js
@@ -27,13 +27,16 @@ function operate (x,y, operator)
     else {console.log('ERROR: unsupported operator')}
 }
 
+let display = document.getElementById('displayText');
 function updateDisplay()
 {
     display.innerText = displayValue;
 }
 
 let displayValue = "";
-let display = document.getElementById('displayText');
+let firstOperand = "";
+let operator = "";
+let secondOperand = "";
 let digits = document.getElementsByClassName('digit');
 for (let i = 0; i < digits.length; i++) 
 {
@@ -41,8 +44,43 @@ for (let i = 0; i < digits.length; i++)
     digits[i].addEventListener('click',() => {
         // console.log(displayValue);
         displayValue = displayValue + number
+        // update parameters
+        if (operator == "")
+        {
+            firstOperand += number;
+        }
+        else
+        {
+            secondOperand += number;
+        }
         updateDisplay();
     });
 }
+let operators = document.getElementsByClassName('operator');
+for (let i = 0; i < operators.length; i++) 
+{
+    operators[i].addEventListener('click',() => {
+        operator = operators[i].innerText;
+        console.log(operator);
+        displayValue = displayValue + operator
+        updateDisplay();
+    });
+}
+let clearButton = document.getElementsByClassName('clear');
+clearButton[0].addEventListener('click', () => {
+    displayValue = "";
+    firstOperand = "";
+    operator = ""
+    secondOperand = "";
+});
+let equalsButton = document.getElementsByClassName('equals');
+equalsButton[0].addEventListener('click', () => {
+    displayValue = operate(parseInt(firstOperand), parseInt(secondOperand), operator);
+    updateDisplay();
+    //reset values while allowing for 'chaining' operations using result
+    firstOperand = displayValue;
+    operator = ""
+    secondOperand = "";
+});
 updateDisplay();
 
