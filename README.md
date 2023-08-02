8ce7503861bb8213e70b3ef91d19dd0e1270e9b1
michael-chung-mc <chung.michael.mc@gmail.com>
2023-08-02 15:24:20 -0400

    Finish assignment

--- README.md
+++ README.md
@@ -17,6 +17,12 @@
 ## About The Project
 The Odin Project Form Validation Assignment Details:
 
+<p float="left">
+    <img src="/assets/example.png" width="30%" />
+    <img src="/assets/example__invalid.png" width="30%" />
+    <img src="/assets/example__valid.png" width="30%" />
+</p>
+
 Build a browser form which collects Email, Country, Zip Code, Password and Password Confirmation fields. It should use live inline validation to inform the user whether a field is properly filled in or not. That means highlighting a field red and providing a helpful error message until it has been filled in properly.
 
 The form doesn’t need to actually submit, but you should give an error message if the button is pushed with any active errors or unfilled required fields. For the sake of this lesson, make sure all of the validation occurs in the JavaScript file. If all is well and the form is “submitted”, give the user a high five.
@@ -30,10 +36,11 @@ The form doesn’t need to actually submit, but you should give an error message
 - [x] Set up a blank HTML document
 - [x] Think about how you would set up the different form elements and their accompanying validators. What objects and functions will you need? A few minutes of thought can save you from wasting an hour of coding. The best thing you can do is whiteboard the entire solution before even touching the computer.
 - [x] Write the simple form elements.
-- [ ] Add the JavaScript code that checks validation as the user progresses through the form. When a user leaves a form field, it should automatically validate that field.
-- [ ] Test out all possible cases.
-- [ ] Don’t forget to style validations with CSS by using the :valid and :invalid pseudo-classes!
+- [x] Add the JavaScript code that checks validation as the user progresses through the form. When a user leaves a form field, it should automatically validate that field.
+- [x] Test out all possible cases.
+- [x] Don’t forget to style validations with CSS by using the :valid and :invalid pseudo-classes!
 
 ## Acknowledgements
 * [The Odin Project](https://www.theodinproject.com/)
-<p align="right">(<a href="#readme-top">backt o top </a>)</p>
+* [Google Materials & Symbols](fonts.google.com)
+<p align="right">(<a href="#readme-top">back to top </a>)</p>
--- main.js
+++ main.js
@@ -1,33 +1,164 @@
-let inputPass = ""
+//color
+let colorDark = "000103";
+let colorMid = "333138";
+let colorLight = "515052";
+let colorWhite = "FFFFFA";
+let colorError = "FF312E";
 
-function validateEmail (value) {
-    return value.match("[a-z]*[A-Z]*\.[a-z]");
+// asset
+let pathImageO = "./assets/google_fonts__radio_button_unchecked_FILL0_wght400_GRAD0_opsz48.png"
+let pathImageX = "./assets/google_fonts__cancel_FILL0_wght400_GRAD0_opsz48.png"
+let pathImageY = "./assets/google_fonts__check_circle_FILL0_wght400_GRAD0_opsz48.png"
+
+//init
+let valid = false;
+let inputPass = ""
+const imgO = document.createElement("img");
+imgO.src = pathImageO;
+
+//check
+function checkEmail (value) {
+    return value.match(/.*\@.*\.[a-z]{3}/);
 }
-function validateCountry (country) {
-    return value.match("[a-z]*[A-Z]*");
+function checkCountry (value) {
+    return value.match(/[a-z]*/);
 }
-function validateZipCode (value) {
-    return value.match("\d\d\d\d\d\d");
+function checkZipCode (value) {
+    return value.match(/^\d{5}/);
 }
-function validatePassword (value) {
+function checkPassword (value) {
+    return true;
+}
+function checkPasswordConfirm (value) {
     return value == inputPass;
 }
 
-
-// main ()
-
-document.getElementById("inputEmail").addEventListener("exit", (e)=> {
-    validateEmail(e.target.value);
-})
-document.getElementById("inputCountry").addEventListener("exit", (e)=> {
-    validateCountry(e.target.value);
-})
-document.getElementById("inputZipCode").addEventListener("exit", (e)=> {
-    validateZipCode(e.target.value);
-})
-document.getElementById("inputPassword").addEventListener("exit", (e)=> {
+//validate
+function validateEmail (value) {
+    if (checkEmail(value))
+    {
+        // :valid
+        console.log("email confirm");
+        inputEmail.setCustomValidity("");
+        // image
+        while(inputEmailMark.firstChild) {inputEmailMark.removeChild(inputEmailMark.firstChild);}
+        const img = document.createElement("img");
+        img.src = pathImageY;
+        inputEmailMark.appendChild(img);
+    }
+    else
+    {
+        // :valid
+        console.log("invalid email");
+        inputEmail.setCustomValidity("invalid");
+        // image
+        while (inputEmailMark.firstChild) {inputEmailMark.removeChild(inputEmailMark.firstChild);}
+        const img = document.createElement("img");
+        img.src = pathImageX;
+        inputEmailMark.appendChild(img);
+    }
+}
+function validateCountry(value) {
+    if (checkCountry(value))
+    {
+        // :valid
+        console.log("country confirm");
+        inputCountry.setCustomValidity("");
+        // image
+        while (inputCountryMark.firstChild) {inputCountryMark.removeChild(inputCountryMark.firstChild);}
+        const img = document.createElement("img");
+        img.src = pathImageY;
+        inputCountryMark.appendChild(img);
+    }
+    else
+    {
+        // :valid
+        console.log("invalid zip");
+        inputCountry.setCustomValidity("invalid");
+        // image
+        while (inputCountryMark.firstChild) {inputCountryMark.removeChild(inputCountryMark.firstChild);}
+        const img = document.createElement("img");
+        img.src = pathImageY;
+        inputCountryMark.appendChild(img);
+    }
+}
+function validateZipCode (value) {
+    if (checkZipCode(value))
+    {
+        // :valid
+        console.log("zip confirm");
+        inputZipCode.setCustomValidity("");
+        // image
+        while (inputZipCodeMark.firstChild) {inputZipCodeMark.removeChild(inputZipCodeMark.firstChild);}
+        const img = document.createElement("img");
+        img.src = pathImageY;
+        inputZipCodeMark.appendChild(img);
+    }
+    else
+    {
+        // :valid
+        console.log("invalid zip");
+        inputZipCode.setCustomValidity("invalid");
+        // image
+        while (inputZipCodeMark.firstChild) {inputZipCodeMark.removeChild(inputZipCodeMark.firstChild);}
+        const img = document.createElement("img");
+        img.src = pathImageX;
+        inputZipCodeMark.appendChild(img);
+    }
+}
+function validatePasswordConfirm (value)
+{
+    if (checkPasswordConfirm(value))
+    {
+        // :valid
+        console.log("pass confirm");
+        inputPassConfirm.setCustomValidity("");
+        // image
+        while(inputPassConfirmMark.firstChild) {inputPassConfirmMark.removeChild(inputPassConfirmMark.firstChild);}
+        const img = document.createElement("img");
+        img.src = pathImageY;
+        inputPassConfirmMark.appendChild(img);
+    }
+    else
+    {
+        // :valid
+        console.log("invalid pass confirm");
+        inputPassConfirm.setCustomValidity("invalid");
+        // image
+        while(inputPassConfirmMark.firstChild) {inputPassConfirmMark.removeChild(inputPassConfirmMark.firstChild);}
+        const img = document.createElement("img");
+        img.src = pathImageX;
+        inputPassConfirmMark.appendChild(img);
+    }
+}
+// email
+const inputEmail = document.getElementById("inputEmail")
+const inputEmailMark = document.getElementById("inputEmailMark")
+inputEmail.addEventListener("focusout", (e)=> { validateEmail(e.target.value); })
+// country
+const inputCountry = document.getElementById("inputCountry");
+const inputCountryMark = document.getElementById("inputCountryMark");
+inputCountry.addEventListener("focusout", (e)=> {validateCountry(e.target.value);})
+// zipcode
+const inputZipCode = document.getElementById("inputZipCode");
+const inputZipCodeMark = document.getElementById("inputZipCodeMark");
+inputZipCode.addEventListener("focusout", (e)=> { validateZipCode(e.target.value); })
+// password
+document.getElementById("inputPassword").addEventListener("focusout", (e)=> {
     inputPass = e.target.value;
 })
-document.getElementById("inputPasswordConfirmation").addEventListener("exit", (e)=> {
-    validatePassword(e.target.value);
+// password confirmation
+const inputPassConfirm = document.getElementById("inputPasswordConfirmation");
+const inputPassConfirmMark = document.getElementById("inputPasswordConfirmationMark")
+inputPassConfirm.addEventListener("focusout", (e)=> { validatePasswordConfirm(e.target.value); })
+const submitButton = document.getElementById("submitButton");
+submitButton.addEventListener("click", ()=> {
+    if (valid)
+    {
+
+    }
+    else
+    {
+        alert("Errors in form");
+    }
 })
