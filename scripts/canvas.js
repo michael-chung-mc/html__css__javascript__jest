ee60a6c9561980e95683cc8129c5f9752448ad67
michael-chung-mc <chung.michael.mc@gmail.com>
2023-09-08 17:42:32 -0400

    SET Basic ppm viewing functionality

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -13,14 +13,24 @@ function Canvas()
     let pixels;
     //let id = ctx.getImageData(0,0, canvasWidth, canvasHeight);
     function reset (w, h) {
-        canvas = document.createElement("canvas");
+        let content = document.getElementById("content");
+        if (content.firstChild)
+        {
+            ctx.clearRect(0,0,canvas.width,canvas.height);
+        }
+        else
+        {
+            canvas = document.createElement("canvas");
+            content.appendChild(canvas);
+        }
         width = w;
         height = h;
+        canvas.width = width;
+        canvas.height = height;
         ctx = canvas.getContext("2d");
         ctx.createImageData(width, height);
         image = ctx.getImageData(0,0,width,height);
         pixels = image.data;
-        document.getElementById("content").appendChild(canvas);
     }
     function setDepth(x)
     {
@@ -28,10 +38,11 @@ function Canvas()
     }
     function setPixelArray(i, r, g, b, a)
     {
-        pixels[i] = r;
-        pixels[i+1] = g;
-        pixels[i+2] = b;
-        pixels[i+3] = a;
+        var offsetIndex = i * 4;
+        pixels[offsetIndex] = r;
+        pixels[offsetIndex+1] = g;
+        pixels[offsetIndex+2] = b;
+        pixels[offsetIndex+3] = a;
     }
     function setPixel(x, y, r, g, b, a)
     {
@@ -71,113 +82,135 @@ function Canvas()
     }
 }
 
-function compareString(x, y)
-{
-    if (x.length != y.length)
-    {
-        return false;
+function Main () {
+    let cvs = Canvas();
+    let alpha = 255;
+    function init () {
+        const inputElement = document.getElementById("input");
+        inputElement.addEventListener("change", handleFiles, false);
     }
-    for (let i = 0; i < x.length; i++)
+    function compareString(x, y)
     {
-        if (x.charCodeAt(i) != y.charCodeAt(i))
+        if (x.length != y.length)
         {
             return false;
         }
+        for (let i = 0; i < x.length; i++)
+        {
+            if (x.charCodeAt(i) != y.charCodeAt(i))
+            {
+                return false;
+            }
+        }
+        return true;
     }
-    return true;
-}
-
-function handleFiles() {
-    const fileList = this.files;
-    let file = fileList[0];
-    let reader = new FileReader();
-    reader.readAsText(file);
-    reader.onload = function () {
-        let r = null;
-        let b = null;
-        let g = null;
-        let a = null;
-        let data = reader.result;
-        let lines = data.split('\n');
-        let headerLength = 0;
-        for (let i = 0; i < lines.length; i++)
-        {
-            //console.log(lines[i]);
-            if (i == 0 && !compareString(lines[i].trim(),"P3"))
+    function handleFiles() {
+        const fileList = this.files;
+        let file = fileList[0];
+        let reader = new FileReader();
+        reader.readAsText(file);
+        reader.onload = function () {
+            let r = null;
+            let b = null;
+            let g = null;
+            let a = null;
+            let data = reader.result;
+            let lines = data.split('\n');
+            let headerLength = 0;
+            for (let i = 0; i < lines.length; i++)
             {
-                console.log(`unsupported ppm header version <${lines[i]}>`);
-                return;
-            }
-            else if (i == 1)
-            {
-                let dimensions = lines[i].trim().split(/\s/);
-                let width = parseInt(dimensions[0].trim());
-                let height = parseInt(dimensions[1].trim());
-                if (width == NaN || height == NaN)
+                //console.log(lines[i]);
+                if (i == 0 && !compareString(lines[i].trim(),"P3"))
                 {
-                    console.log(`incorrect ppm header dimensions <${lines[i]}>`);
+                    console.log(`unsupported ppm header version <${lines[i]}>`);
                     return;
                 }
-                cvs.reset(width, height);
-            }
-            else if (i == 2)
-            {
-                let colorDepth = parseInt(lines[i].trim());
-                if (colorDepth == NaN)
+                else if (i == 1)
                 {
-                    console.log(`incorrect ppm header color <${lines[i]}>`);
-                    return;
+                    let dimensions = lines[i].trim().split(/\s/);
+                    let width = parseInt(dimensions[0].trim());
+                    let height = parseInt(dimensions[1].trim());
+                    if (width == NaN || height == NaN)
+                    {
+                        console.log(`incorrect ppm header dimensions <${lines[i]}>`);
+                        return;
+                    }
+                    cvs.reset(width, height);
                 }
-                cvs.setDepth(colorDepth);
+                else if (i == 2)
+                {
+                    let colorDepth = parseInt(lines[i].trim());
+                    if (colorDepth == NaN)
+                    {
+                        console.log(`incorrect ppm header color <${lines[i]}>`);
+                        return;
+                    }
+                    cvs.setDepth(colorDepth);
+                    headerLength += lines[i].length+1;
+                    break;
+                }
                 headerLength += lines[i].length+1;
-                break;
             }
-            headerLength += lines[i].length;
-        }
-        let colors = data.substring(headerLength, data.length);
-        //console.log(colors);
-        let concat = colors.replace(/\n/, ' ');
-        let index = 0;
-        while (index < concat.length)
-        {
-            r = concat[index];
-            g = concat[index+1];
-            b = concat[index+2];
-            a = concat[index+3];
-            cvs.setPixelArray(index/4, r, g, b, a);
-            index += 4;
-            //console.log(`${r}${g}${b}${a}-`)
-        }
-        //console.log(concat);
-
-            // else
+            let colors = data.substring(headerLength);
+            //console.log(colors);
+            let concat = colors.replace(/\s+/g, ' ');
+            //console.log(concat);
+            let split = concat.split(/\s/);
+            //console.log(split);
+            let index = 0;
+            while (index < split.length)
+            {
+                let r = split[index];
+                let g = split[index+1];
+                let b = split[index+2];
+                cvs.setPixelArray(index/3, r, g, b, alpha);
+                //console.log(`${index}=${index/3}==(${r},${g},${b},${alpha})`)
+                index += 3;
+            }
+            // for (let i = 0; i < cvs.width; i++)
             // {
-            //     let colors = lines[i].trim().split(/\s/);
-            //     let index = 0;
-            //     while (index < colors.length)
+            //     for (let j =0; j < cvs.height;j++)
             //     {
-            //         r = colors[index++];
-            //         if (index > colors.length)
-            //         {
-            //             g = null;
-            //             b = null;
-            //             a = null;
-            //             break;
-            //         }
-
-            //         g = colors[index++];
-            //         b = colors[index++];
-            //         a = colors[index++];
-
-            //         cvs.setPixel(i, index, colors[index], colors[index+1], colors[index+2], colors[index+3]);
-            //         index += 4;
+            //         let r = split[i+j*cvs.width];
+            //         let g = split[i+1+j*cvs.width];
+            //         let b = split[i+2+j*cvs.width];
+            //         cvs.setPixel(i,j,r,g,b,a);
+            //         console.log(`${i}=${j}==(${r},${g},${b},${alpha})`)
             //     }
             // }
-        //}
-        cvs.display();
-    };
-}
+            //console.log(split);
+    
+                // else
+                // {
+                //     let colors = lines[i].trim().split(/\s/);
+                //     let index = 0;
+                //     while (index < colors.length)
+                //     {
+                //         r = colors[index++];
+                //         if (index > colors.length)
+                //         {
+                //             g = null;
+                //             b = null;
+                //             a = null;
+                //             break;
+                //         }
+    
+                //         g = colors[index++];
+                //         b = colors[index++];
+                //         a = colors[index++];
+    
+                //         cvs.setPixel(i, index, colors[index], colors[index+1], colors[index+2], colors[index+3]);
+                //         index += 4;
+                //     }
+                // }
+            //}
+            cvs.display();
+        };
+    }
+    return {
+        init,
+    }
+};
 
-let cvs = Canvas();
-const inputElement = document.getElementById("input");
-inputElement.addEventListener("change", handleFiles, false);
+let main = Main()
+main.init();
